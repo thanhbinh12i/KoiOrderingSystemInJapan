9ef3a2167a0530335508ec191e517105ccd2580a
thinhdabezt <thinhdinhtran0605@gmail.com>
2024-09-23 22:31:52 +0700

    UpdateAndViewAccount

--- BackEnd/Koi_Ordering_System/Project_SWP391/Controllers/AccountController.cs
+++ BackEnd/Koi_Ordering_System/Project_SWP391/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
 using Project_SWP391.Dtos.Account;
 using Project_SWP391.Interfaces;
 using Project_SWP391.Model;
@@ -98,6 +99,83 @@ namespace Project_SWP391.Controllers
                     Token = _tokenService.CreateToken(user)
                 }
             );
+        }
+        [HttpPut("update")]
+        public async Task<IActionResult> Update([FromBody] UpdateUserDTO updateUser, [FromHeader] string id)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.Id == id);
+            if (user == null) return NotFound("User not found");
+
+            var usernameExists = await _userManager.Users.AnyAsync(x => x.UserName == updateUser.UserName && x.Id != id);
+            if (usernameExists) return Conflict("Username already in use by another user");
+
+            var emailExists = await _userManager.Users.AnyAsync(x => x.Email == updateUser.Email && x.Id != id);
+            if (emailExists) return Conflict("Email already in use by another user");
+
+            string formattedPhoneNumber = FormatPhoneNumber(updateUser.PhoneNumber);
+            if (formattedPhoneNumber == null) return BadRequest("Invalid phone number format");
+
+            if (!string.IsNullOrEmpty(updateUser.Password))
+            {
+                var removePasswordResult = await _userManager.RemovePasswordAsync(user);
+                if (!removePasswordResult.Succeeded)
+                {
+                    return BadRequest(removePasswordResult.Errors);
+                }
+
+                var addPasswordResult = await _userManager.AddPasswordAsync(user, updateUser.Password);
+                if (!addPasswordResult.Succeeded)
+                {
+                    return BadRequest(addPasswordResult.Errors);
+                }
+            }
+
+            user.UserName = updateUser.UserName;
+            user.Email = updateUser.Email;
+            user.Gender = updateUser.Gender;
+            user.Address = updateUser.Address;
+            user.FullName = updateUser.FullName;
+            user.PhoneNumber = formattedPhoneNumber;
+            user.DateOfBirth = updateUser.DateOfBirth;
+
+            var result = await _userManager.UpdateAsync(user);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok(new UpdatedUserDTO
+            {
+                UserName = user.UserName,
+                Email = user.Email,
+                FullName = user.FullName,
+                PhoneNumber = user.PhoneNumber,
+                Gender = user.Gender,
+                Address = user.Address,
+                DateOfBirth = user.DateOfBirth,
+                Token = _tokenService.CreateToken(user)
+            });
+        }
+        [HttpGet("view")]
+        public async Task<IActionResult> View([FromHeader] string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound("No user found");
+            return Ok
+                (
+                    new ViewAccountDTO
+                    {
+                        UserName = user.UserName,
+                        Email = user.Email,
+                        Gender = user.Gender,
+                        Address = user.Address,
+                        FullName= user.FullName,
+                        PhoneNumber = user.PhoneNumber,
+                        DateOfBirth = user.DateOfBirth
+                    }
+                );
         }
         private string FormatPhoneNumber(string phoneNumber)
         {
