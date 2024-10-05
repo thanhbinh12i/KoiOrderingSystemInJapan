debfba5345a2b5440bf30c09f4facd62ef2a4e63
Nguyễn Việt <vietbmt19@gmail.com>
2024-10-05 13:26:14 +0700

    update search Tour

--- FrontEnd/swp-frontend/src/pages/Admin/Tour/SearchByName.js
+++ FrontEnd/swp-frontend/src/pages/Admin/Tour/SearchByName.js
@@ -1,16 +1,30 @@
 import React from "react";
-import { Input } from "antd";
-
-const { Search } = Input;
+import { Input, Button } from "antd";
+import { useState } from "react";
 
 function SearchByName({ onSearch, className }) {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(searchValue);
+  };
+
   return (
-    <Search
-      placeholder="Tìm kiếm theo tên tour"
-      onSearch={onSearch}
-      className={className}
-      style={{ width: 200 }}
-    />
+    <div className={`search-container ${className}`}>
+      <Input
+        placeholder="Search by name"
+        value={searchValue}
+        onChange={handleInputChange}
+        className="search-input"
+      />
+      <Button type="primary" onClick={handleSearch} className={className}>
+        Search
+      </Button>
+    </div>
   );
 }
 
