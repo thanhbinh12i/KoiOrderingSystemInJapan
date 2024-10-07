b6066a5ca09980f83c00fe4b1eaf2e9d30e6b66f
Ryan <anhoang800140@gmail.com>
2024-10-07 23:53:39 +0700

    Remove service model

--- BackEnd/Koi_Ordering_System/Project_SWP391/Dtos/Services/CreateServiceDto.cs
+++ BackEnd/Koi_Ordering_System/Project_SWP391/Dtos/Services/CreateServiceDto.cs
@@ -1,9 +1,9 @@
-﻿namespace Project_SWP391.Dtos.Services
-{
-    public class CreateServiceDto
-    {
-        public string ServiceName { get; set; } = string.Empty;
-        public float Price { get; set; }
-        public string Detail { get; set; } = string.Empty;
-    }
-}
+﻿//namespace Project_SWP391.Dtos.Services
+//{
+//    public class CreateServiceDto
+//    {
+//        public string ServiceName { get; set; } = string.Empty;
+//        public float Price { get; set; }
+//        public string Detail { get; set; } = string.Empty;
+//    }
+//}
--- BackEnd/Koi_Ordering_System/Project_SWP391/Dtos/Services/ServiceDto.cs
+++ BackEnd/Koi_Ordering_System/Project_SWP391/Dtos/Services/ServiceDto.cs
@@ -1,10 +1,10 @@
-﻿namespace Project_SWP391.Dtos.Services
-{
-    public class ServiceDto
-    {
-        public int ServiceId { get; set; }
-        public string ServiceName { get; set; } = string.Empty;
-        public float Price { get; set; }
-        public string Detail { get; set; } = string.Empty;
-    }
-}
+﻿//namespace Project_SWP391.Dtos.Services
+//{
+//    public class ServiceDto
+//    {
+//        public int ServiceId { get; set; }
+//        public string ServiceName { get; set; } = string.Empty;
+//        public float Price { get; set; }
+//        public string Detail { get; set; } = string.Empty;
+//    }
+//}
