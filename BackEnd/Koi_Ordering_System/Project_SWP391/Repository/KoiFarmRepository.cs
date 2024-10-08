0dca9b00f237f2ad173d887a5e0d4d54969b9c6f
thanhbinh12ii <nptbinh17092004@gmail.com>
2024-10-08 17:56:57 +0700

    merge develop

--- BackEnd/Koi_Ordering_System/Project_SWP391/Repository/KoiFarmRepository.cs
+++ BackEnd/Koi_Ordering_System/Project_SWP391/Repository/KoiFarmRepository.cs
@@ -1,82 +1,82 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Project_SWP391.Data;
-using Project_SWP391.Dtos.KoiFarm;
-using Project_SWP391.Interfaces;
-using Project_SWP391.Model;
+﻿//using Microsoft.EntityFrameworkCore;
+//using Project_SWP391.Data;
+//using Project_SWP391.Dtos.KoiFarm;
+//using Project_SWP391.Interfaces;
+//using Project_SWP391.Model;
 
-namespace Project_SWP391.Repository
-{
-    public class KoiFarmRepository : IKoiFarmRepository
-    {
-        private readonly ApplicationDBContext _context;
+//namespace Project_SWP391.Repository
+//{
+//    public class KoiFarmRepository : IKoiFarmRepository
+//    {
+//        private readonly ApplicationDBContext _context;
 
-        public KoiFarmRepository(ApplicationDBContext context)
-        {
-            _context = context;
-        }
-        public async Task<KoiFarm> CreateAsync(KoiFarm koiFarmModel)
-        {
-            await _context.KoiFarms.AddAsync(koiFarmModel);
-            await _context.SaveChangesAsync();
+//        public KoiFarmRepository(ApplicationDBContext context)
+//        {
+//            _context = context;
+//        }
+//        public async Task<KoiFarm> CreateAsync(KoiFarm koiFarmModel)
+//        {
+//            await _context.KoiFarms.AddAsync(koiFarmModel);
+//            await _context.SaveChangesAsync();
 
-            return koiFarmModel;
-        }
+//            return koiFarmModel;
+//        }
 
-        public async Task<KoiFarm?> DeleteAsync(int farmId)
-        {
-            var koiFarm = await _context.KoiFarms.FindAsync(farmId);
+//        public async Task<KoiFarm?> DeleteAsync(int farmId)
+//        {
+//            var koiFarm = await _context.KoiFarms.FindAsync(farmId);
 
-            if (koiFarm == null)
-            {
-                return null;
-            }
+//            if (koiFarm == null)
+//            {
+//                return null;
+//            }
 
-            _context.KoiFarms.Remove(koiFarm);
-            _context.SaveChanges();
+//            _context.KoiFarms.Remove(koiFarm);
+//            _context.SaveChanges();
 
-            return koiFarm;
-        }
+//            return koiFarm;
+//        }
 
-        public async Task<List<KoiFarm>> GetAllAsync()
-        {
-            return await _context.KoiFarms.Include(c => c.Kois).Include(c => c.FarmImages).ToListAsync();
-        }
+//        public async Task<List<KoiFarm>> GetAllAsync()
+//        {
+//            return await _context.KoiFarms.Include(c => c.Kois).Include(c => c.FarmImages).ToListAsync();
+//        }
 
-        public async Task<KoiFarm?> GetByIdAsync(int farmId)
-        {
-            return await _context.KoiFarms.Include(c => c.Kois).Include(c => c.FarmImages).FirstOrDefaultAsync(x => x.FarmId == farmId);
-        }
-        public async Task<KoiFarm?> GetByNameAsync(string farmName)
-        {
-            return await _context.KoiFarms.Include(c => c.Kois).Include(c => c.FarmImages).FirstOrDefaultAsync(x => x.FarmName == farmName);
-        }
+//        public async Task<KoiFarm?> GetByIdAsync(int farmId)
+//        {
+//            return await _context.KoiFarms.Include(c => c.Kois).Include(c => c.FarmImages).FirstOrDefaultAsync(x => x.FarmId == farmId);
+//        }
+//        public async Task<KoiFarm?> GetByNameAsync(string farmName)
+//        {
+//            return await _context.KoiFarms.Include(c => c.Kois).Include(c => c.FarmImages).FirstOrDefaultAsync(x => x.FarmName == farmName);
+//        }
 
-        public async Task<KoiFarm?> UpdateAsync(int farmId, UpdateKoiFarmDto koiFarmDto)
-        {
-            var koiFarmModel = await _context.KoiFarms.FirstOrDefaultAsync(x => x.FarmId == farmId);
+//        public async Task<KoiFarm?> UpdateAsync(int farmId, UpdateKoiFarmDto koiFarmDto)
+//        {
+//            var koiFarmModel = await _context.KoiFarms.FirstOrDefaultAsync(x => x.FarmId == farmId);
 
-            if (koiFarmModel == null)
-            {
-                return null;
-            }
+//            if (koiFarmModel == null)
+//            {
+//                return null;
+//            }
 
-            koiFarmModel.FarmName = koiFarmDto.FarmName;
-            koiFarmModel.Introduction = koiFarmDto.Introduction;
-            koiFarmModel.Location = koiFarmDto.Location;
-            koiFarmModel.OpenHour = koiFarmDto.OpenHour;
-            koiFarmModel.CloseHour = koiFarmDto.CloseHour;
-            koiFarmModel.Email = koiFarmDto.Email;
-            koiFarmModel.Rating = koiFarmDto.Rating;
-            koiFarmModel.Hotline = koiFarmDto.Hotline;
+//            koiFarmModel.FarmName = koiFarmDto.FarmName;
+//            koiFarmModel.Introduction = koiFarmDto.Introduction;
+//            koiFarmModel.Location = koiFarmDto.Location;
+//            koiFarmModel.OpenHour = koiFarmDto.OpenHour;
+//            koiFarmModel.CloseHour = koiFarmDto.CloseHour;
+//            koiFarmModel.Email = koiFarmDto.Email;
+//            koiFarmModel.Rating = koiFarmDto.Rating;
+//            koiFarmModel.Hotline = koiFarmDto.Hotline;
 
 
-            await _context.SaveChangesAsync();
+//            await _context.SaveChangesAsync();
 
-            return koiFarmModel;
-        }
-        public Task<bool> ExistKoiFarm(int farmId)
-        {
-            return _context.KoiFarms.AnyAsync(s => s.FarmId == farmId);
-        }
-    }
-}
+//            return koiFarmModel;
+//        }
+//        public Task<bool> ExistKoiFarm(int farmId)
+//        {
+//            return _context.KoiFarms.AnyAsync(s => s.FarmId == farmId);
+//        }
+//    }
+//}
