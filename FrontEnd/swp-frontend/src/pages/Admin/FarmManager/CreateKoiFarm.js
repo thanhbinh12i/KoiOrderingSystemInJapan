b4b9e2ab12ef68781b22b909d1a9a9c9af6d74f0
Nguyễn Việt <vietbmt19@gmail.com>
2024-10-09 11:23:03 +0700

    create farm with img

--- FrontEnd/swp-frontend/src/pages/Admin/FarmManager/CreateKoiFarm.js
+++ FrontEnd/swp-frontend/src/pages/Admin/FarmManager/CreateKoiFarm.js
@@ -1,120 +1,187 @@
 import React, { useState } from "react";
-import { Modal, Form, Input, Button, message, TimePicker } from "antd";
+import {
+  Form,
+  Input,
+  Button,
+  message,
+  TimePicker,
+  Row,
+  Col,
+  Upload,
+} from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 import { post } from "../../../utils/request";
 
-function CreateKoiFarm({ isModalVisible, handleOk, handleCancel }) {
+function CreateKoiFarm() {
   const [form] = Form.useForm();
   const [loading, setLoading] = useState(false);
+  const [fileList, setFileList] = useState([]);
   const [messageApi, contextHolder] = message.useMessage();
 
-  const handleSubmit = async (values) => {
+  const handleFinish = async (values) => {
     try {
       setLoading(true);
       const formattedValues = {
         ...values,
-        openHour: values.openHour ? values.openHour.format("HH:mm") : null,
-        closeHour: values.closeHour ? values.closeHour.format("HH:mm") : null,
+        openHour: values.openHour.format("HH:mm"),
+        closeHour: values.closeHour.format("HH:mm"),
       };
       const response = await post("koiFarm/create", formattedValues);
+
       if (response) {
+        if (fileList.length > 0) {
+          await uploadImages(response.farmId, fileList);
+        }
         form.resetFields();
-        handleOk();
-        messageApi.success("Thêm trang trại cá Koi mới thành công");
+        setFileList([]);
+        messageApi.success("Thêm trang trại koi mới thành công");
       } else {
-        messageApi.error("Thêm trang trại cá Koi không thành công");
+        messageApi.error("Thêm trang trại mới không thành công");
       }
     } catch (error) {
-      messageApi.error("Lỗi: " + error.message);
+      messageApi.error("Lỗi khi thêm trang trại koi mới");
     } finally {
       setLoading(false);
     }
   };
 
+  const uploadImages = async (farmId, files) => {
+    const formData = new FormData();
+    files.forEach((file) => {
+      formData.append("files", file.originFileObj);
+    });
+
+    try {
+      const response = await fetch(
+        `https://localhost:7087/api/farmImage/upload/${farmId}`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
+      if (!response) {
+        throw new Error("Lỗi tải lên hình ảnh");
+      }
+
+      const data = await response.json();
+      messageApi.success("Tải lên hình ảnh thành công");
+      return data.urls;
+    } catch (error) {
+      console.error("Error uploading images:", error);
+      messageApi.error("Lỗi tải lên hình ảnh");
+      throw error;
+    }
+  };
+  const handleFileChange = ({ fileList }) => setFileList(fileList);
+
   return (
     <>
       {contextHolder}
-      <Modal
-        title="Thêm trang trại cá Koi mới"
-        visible={isModalVisible}
-        onCancel={handleCancel}
-        footer={null}
+      <h1>Thêm trang trại cá Koi mới</h1>
+      <Form
+        form={form}
+        layout="vertical"
+        onFinish={handleFinish}
+        initialValues={{ rating: 5 }}
       >
-        <Form
-          form={form}
-          layout="vertical"
-          onFinish={handleSubmit}
-          initialValues={{ rating: 5 }}
-        >
-          <Form.Item
-            label="Tên trang trại"
-            name="farmName"
-            rules={[
-              { required: true, message: "Vui lòng nhập tên trang trại!" },
-            ]}
-          >
-            <Input placeholder="Nhập tên trang trại" />
-          </Form.Item>
-          <Form.Item
-            label="Giới thiệu"
-            name="introduction"
-            rules={[{ required: true, message: "Vui lòng nhập giới thiệu!" }]}
-          >
-            <Input.TextArea placeholder="Nhập giới thiệu" />
-          </Form.Item>
-          <Form.Item
-            label="Địa chỉ"
-            name="location"
-            rules={[{ required: true, message: "Vui lòng nhập địa chỉ!" }]}
-          >
-            <Input placeholder="Nhập địa chỉ" />
-          </Form.Item>
-          <Form.Item
-            label="Giờ mở cửa"
-            name="openHour"
-            rules={[{ required: true, message: "Vui lòng chọn giờ mở cửa!" }]}
-          >
-            <TimePicker format="HH:mm" />
-          </Form.Item>
-          <Form.Item
-            label="Giờ đóng cửa"
-            name="closeHour"
-            rules={[{ required: true, message: "Vui lòng chọn giờ đóng cửa!" }]}
-          >
-            <TimePicker format="HH:mm" />
-          </Form.Item>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              { required: true, message: "Vui lòng nhập email!" },
-              { type: "email", message: "Vui lòng nhập email hợp lệ!" },
-            ]}
-          >
-            <Input placeholder="Nhập email" />
-          </Form.Item>
-          <Form.Item
-            label="Đánh giá"
-            name="rating"
-            rules={[{ required: true, message: "Vui lòng nhập đánh giá!" }]}
-          >
-            <Input type="number" min={0} max={5} step={0.1} />
-          </Form.Item>
-          <Form.Item
-            label="Số điện thoại"
-            name="hotline"
-            rules={[
-              { required: true, message: "Vui lòng nhập số điện thoại!" },
-            ]}
-          >
-            <Input placeholder="Nhập số điện thoại" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" loading={loading}>
-              Thêm
-            </Button>
-          </Form.Item>
-        </Form>
-      </Modal>
+        <Row gutter={20}>
+          <Col span={24}>
+            <Form.Item
+              label="Tên trang trại"
+              name="farmName"
+              rules={[
+                { required: true, message: "Vui lòng nhập tên trang trại!" },
+              ]}
+            >
+              <Input placeholder="Nhập tên trang trại" />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              label="Giới thiệu"
+              name="introduction"
+              rules={[{ required: true, message: "Vui lòng nhập giới thiệu!" }]}
+            >
+              <Input.TextArea rows={4} placeholder="Nhập giới thiệu" />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              label="Địa chỉ"
+              name="location"
+              rules={[{ required: true, message: "Vui lòng nhập địa chỉ!" }]}
+            >
+              <Input placeholder="Nhập địa chỉ" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Giờ mở cửa"
+              name="openHour"
+              rules={[{ required: true, message: "Vui lòng chọn giờ mở cửa!" }]}
+            >
+              <TimePicker format="HH:mm" style={{ width: "100%" }} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Giờ đóng cửa"
+              name="closeHour"
+              rules={[
+                { required: true, message: "Vui lòng chọn giờ đóng cửa!" },
+              ]}
+            >
+              <TimePicker format="HH:mm" style={{ width: "100%" }} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                { required: true, message: "Vui lòng nhập email!" },
+                { type: "email", message: "Vui lòng nhập email hợp lệ!" },
+              ]}
+            >
+              <Input placeholder="Nhập email" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Số điện thoại"
+              name="hotline"
+              rules={[
+                { required: true, message: "Vui lòng nhập số điện thoại!" },
+              ]}
+            >
+              <Input placeholder="Nhập số điện thoại" />
+            </Form.Item>
+          </Col>
+
+          <Col span={24}>
+            <Form.Item label="Hình ảnh trang trại" name="farmImage">
+              <Upload
+                listType="picture"
+                maxCount={1}
+                fileList={fileList}
+                onChange={handleFileChange}
+                beforeUpload={() => false}
+              >
+                <Button icon={<UploadOutlined />}>Tải lên hình ảnh</Button>
+              </Upload>
+            </Form.Item>
+          </Col>
+
+          <Col span={24}>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" loading={loading}>
+                Tạo mới
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
     </>
   );
 }
--- FrontEnd/swp-frontend/src/pages/Admin/FarmManager/FarmDetail.js
+++ FrontEnd/swp-frontend/src/pages/Admin/FarmManager/FarmDetail.js
@@ -1,10 +1,83 @@
 import GoBack from "../../../components/GoBack";
-
+import { get } from "../../../utils/request";
+import { Image } from "antd";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 function FarmDetail() {
-      return (
-            <>
-                  <GoBack />
-            </>
-      )
+  const params = useParams();
+  const [data, setData] = useState();
+  //   const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchApi = async () => {
+      const response = await get(`koiFarm/view/${params.id}`);
+      if (response) {
+        setData(response);
+      }
+    };
+    //     const fetchKoiFarmImages = async () => {
+    //       const response = await get(`farmImage/view/${params.id}`);
+    //       if (response) {
+    //         setImages(response);
+    //       }
+    //     };
+    const fetchData = async () => {
+      setLoading(true);
+      await Promise.all([
+        fetchApi(),
+        // , fetchKoiFarmImages()
+      ]);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [params.id]);
+  console.log(data);
+  return (
+    <>
+      <GoBack />
+      {data && (
+        <>
+          <h1>{data.farmName}</h1>
+          <div className="mb-20">
+            Giới thiệu: <strong>{data.introduction}</strong>
+          </div>
+          <div className="mb-20">
+            Địa chỉ: <strong>{data.location}</strong>
+          </div>
+          <div className="mb-20">
+            Giờ mở cửa: <strong>{data.openHour}</strong>
+          </div>
+          <div className="mb-20">
+            Giờ đóng cửa: <strong>{data.closeHour}</strong>
+          </div>
+          <div className="mb-20">
+            Email liên hệ: <strong>{data.email}</strong>
+          </div>
+          <div className="mb-20">
+            Hotline <strong>{data.hotline}</strong>
+          </div>
+          <div className="mb-20">
+            {data && (
+              <div className="mb-20">
+                <h2>Hình ảnh</h2>
+                <Image.PreviewGroup>
+                  {data.farmImages.map((image, index) => (
+                    <Image
+                      key={image.id || index}
+                      src={`/api/farmImage/view/${image.farmImageId}`}
+                      alt={`Koi farm ${index + 1}`}
+                      width={200}
+                      className="mr-10 mb-10"
+                    />
+                  ))}
+                </Image.PreviewGroup>
+              </div>
+            )}
+          </div>
+        </>
+      )}
+    </>
+  );
 }
 export default FarmDetail;
