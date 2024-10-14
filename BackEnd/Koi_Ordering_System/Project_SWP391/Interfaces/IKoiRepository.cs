572077bda1f36997aa3eaa5d94937da90ecd4593
thinhdabezt <thinhdinhtran0605@gmail.com>
2024-10-14 13:29:34 +0700

    View koi by variety or farm

--- BackEnd/Koi_Ordering_System/Project_SWP391/Interfaces/IKoiRepository.cs
+++ BackEnd/Koi_Ordering_System/Project_SWP391/Interfaces/IKoiRepository.cs
@@ -7,7 +7,9 @@ namespace Project_SWP391.Interfaces
     {
         Task<List<Koi>> GetAllAsync();
         Task<Koi?> GetByIdAsync(int id);
-        Task<Koi?> GetByNameAsync(string name);
+        Task<List<Koi>> GetByNameAsync(string name);
+        Task<List<Koi>?> GetByVarietyAsync(string varietyName);
+        Task<List<Koi>?> GetByFarmAsync(string farmName);
         Task<Koi> CreateAsync(Koi koi);
         Task<Koi?> UpdateAsync(int id, UpdateKoiDto variety);
         Task<Koi?> DeleteAsync(int id);
